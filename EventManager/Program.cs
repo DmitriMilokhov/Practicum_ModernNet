3ffb5c0a6df4a8cb4ae601a0f7895683586484a9
Dmitry Milokhov <kobi23@yandex.ru>
2026-03-31 10:37:38 +0300

    [Sprint1] - Added Initial Project

---
+++ EventManager/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+var isDevelopment = builder.Environment.IsDevelopment();
+
+//before Build
+if(isDevelopment)
+{
+    builder.Host.UseDefaultServiceProvider(options =>
+    {
+        options.ValidateScopes = true;
+        options.ValidateOnBuild = true;
+    });
+}
+
+//services
+builder.Services.AddSwaggerGen();
+
+//after build
+var app = builder.Build();
+
+
+if(isDevelopment)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
